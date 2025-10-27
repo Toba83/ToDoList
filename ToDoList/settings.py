f8ce291273ad53d1becb57e8c52428413d0a9be5
Toba83 <nima.Toba83@gmail.com>
2025-10-27 13:52:28 +0330

    Prepare project for Vercel deployment

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rko87u@$8h#qcrr-$6*t77!83n2ysh$8&epm-z3(8rmog)w0^v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
